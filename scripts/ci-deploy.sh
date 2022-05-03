78b14752160c97f026fed968ddddd3eaadcff476
Zachary Kayyali <zkayyali@redhat.com>
2022-05-03 15:10:17 -0400

    link to openshift pipeline

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -20,12 +20,17 @@ echo "$_GITCOMMIT\n"
 #     tar xz && sudo mv yq_linux_amd64 /usr/bin/yq >/dev/null
 # fi
 
+mkdir -p samples/
+cd samples/
+git clone --single-branch --branch "${_GITBRANCH}"  https://github.com/${_GITREPO}.git
+
 echo "== Deploying ACM-AAP-AAS-Operations to ${_cloud_provider}..."
 
-# # Check if cloud provider is valid. Perhaps support AKS next.
-# if [[ "$_cloud_provider" == "aws" ]]; then
-#   make deploy-dev
-# else
-#   echo "Invalid cloud provider. Exiting."
-#   exit 1
-# fi
+# Check if cloud provider is valid. Perhaps support AKS next.
+if [[ "$_cloud_provider" == "aws" ]]; then
+    oc login ${_AWS_CLUSTERPOOL_API_URL} --insecure-skip-tls-verify --token="${_AWS_CLUSTERPOOL_TOKEN}"
+    oc project managed-services
+else
+  echo "Invalid cloud provider. Exiting."
+  exit 1
+fi
